2d29ffaded7dcdaf5df598079f7837f41882fe6a
Karl Bjorkman <karlbjorkman15@gmail.com>
2022-10-26 20:33:39 -0400

    Added pd.melt() and .duplicated() methods

--- data_cleaning2.py
+++ data_cleaning2.py
@@ -10,8 +10,29 @@ for file in files:
 
 people = pd.concat(df_list, ignore_index = True)
 
-print(people)
-print()
-print("This dataframe is {length} rows long.".format(length = len(people)))
-print()
-print(people.info())
+# print(people)
+# print()
+# print("This dataframe is {length} rows long.".format(length = len(people)))
+# print()
+# print(people.info())
+
+df = pd.read_csv('file1.csv')
+
+df = pd.melt(
+    frame = df, # dataframe you want to melt
+    id_vars = ['name', 'gender', 'age'], # column(s) of the old df you want to keep
+    value_vars = ['old_column1', 'old_column2'], # column(s) of the old df you want to turn into variables
+    value_name = 'new_value_column', # new column that stores values
+    var_name = 'new_variable_column' # new column that stores what were previously variables
+)
+df.columns(['Column 1', 'Column 2', 'Column 3'])
+
+# Dealing with Duplicates
+
+duplicates = df.duplicated()
+print(duplicates.value_counts())
+
+df = df.drop_duplicates()
+duplicates = df.duplicated()
+print(duplicates.value_counts())
+
